fd5ffa08e3971d0b34773ea40ac782aa4a8cedce
Mario Orlandi <morlandi@brainstorm.it>
2018-04-21 19:55:14 +0200

    bump to version 1.0.0

--- setup.py
+++ setup.py
@@ -5,17 +5,13 @@ def readme():
         return f.read()
 
 setup(name='sitesync',
-      version='0.2.0',
+      version='1.0.0',
       description='Script to sync db and/or data from remote host',
       long_description=readme(),
       classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Framework :: Django :: 1.x',
-        'Framework :: Django :: 2.x',
-        'Framework :: Wordpress',
         'Topic :: Database :: Database Engines/Servers',
         'Topic :: Internet',
       ],
